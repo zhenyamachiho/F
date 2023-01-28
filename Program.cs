37c2037a54619e3386cb5ba3eed0ade52d4a6743
Zhenya <zhenya.machiho1@gmail.com>
2023-01-28 16:20:19 +0300

    GetStringFromArray

--- Program.cs
+++ Program.cs
@@ -13,3 +13,29 @@
 
 string[] arr = GetArray();
 
+void GetSrtingFromArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] arrayLessThree = new string[count];
+
+    int a = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arrayLessThree[a] = arr[i];
+            a++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Массив из строк длина которых меньше либо равна 3 символа:");
+    Console.WriteLine(string.Join(", ", arrayLessThree));
+}
