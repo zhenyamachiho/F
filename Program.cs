a2f144661478d5b13eb9c5bf3fc4c58e945c3f2c
Zhenya <zhenya.machiho1@gmail.com>
2023-01-28 16:17:11 +0300

    GetArray

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] GetArray()
+{
+    Console.WriteLine("Введите количество строк:");
+    int arrayLenght = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[arrayLenght];
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        Console.WriteLine($"Введите строку №{i + 1}:");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
